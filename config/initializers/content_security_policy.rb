c570c4ba7ae4eaa509b88458be036ed26eb98f1c
Justin Searls <searls@gmail.com>
2018-06-30 09:21:52 -0400

    webpacker:install said to do this

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -14,6 +14,10 @@
 
 #   # Specify URI for violation reports
 #   # policy.report_uri "/csp-violation-report-endpoint"
+#
+#
+#   # webpacker:install told me to do this.
+# policy.connect_src :self, :https, "http://localhost:3035", "ws://localhost:3035" if Rails.env.development?
 # end
 
 # If you are using UJS then enable automatic nonce generation
