05465e61f67061c9708d929522aa9a3a3ffc4931
Justin Searls <searls@gmail.com>
2018-06-30 10:00:21 -0400

    Import our root CSS/Sass file
    This way, Webpack's file-loader plugin (again, pre-configured by
    webpacker) knows to add it to public/packs/manifest.json.
    As a result, the CSS pack becomes referenceable elsewhere in the app,
    most usefully so we can create a <link> tag to it in our
    view/layouts/application.html.erb

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
+import '../css/application.scss'
 import testDoubleSvg from '../images/test-double.svg'
 
 document.addEventListener('DOMContentLoaded', () => {
